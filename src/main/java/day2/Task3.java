c1f9a8cbe6f31c081324b6f0af2796aafa6da517
Woodpecker <agubaevt@mail.ru>
2022-02-23 03:49:09 +0600

    23/02/2022 3:46:38.92

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,23 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+//      3. Реализовать программу №2, используя цикл while.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a >= b){
+            System.out.println("Некорректный ввод");
+        }else{
+            int i = a;
+            while (i <= b){
+                i+=1;
+                if(i % 5 == 0 && i % 10 != 0){
+                    System.out.print(i + " ");
+                }
+            }
+        }
     }
 }
