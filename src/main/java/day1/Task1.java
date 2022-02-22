0f93bf0cd26a38730a026376495be500904a85b7
Woodpecker <agubaevt@mail.ru>
2022-02-23 02:51:47 +0600

    23/02/2022 2:50:31.66

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,15 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+/*
+        1. Вывести на экран слово “JAVA”, в строку, чтобы оно повторилось 10 раз, используя цикл while.
+        Вывод в консоль должен быть таким:
+        JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA
+*/
+        int i = 0;
+        while (i < 10){
+            System.out.print("JAVA ");
+            i++;
+        }
     }
 }
--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -1,7 +1,30 @@
 package day1;
 
+import java.util.Scanner;
+
 public class Task6 {
     public static void main(String[] args) {
-
+//        6. Объявите переменную типа int, имя переменной - k. Присвойте этой переменной какую-нибудь цифру от 1 до 9. Используя цикл на ваше усмотрение (for или while), выведите в консоль таблицу умножения для этой цифры в следующем формате:
+//        1 x k = …
+//        2 x k = …
+//        3 x k = …
+//……
+//        9 x k = …
+//
+//
+//        Пример:
+//        k = 3
+//
+//        Вывод в консоль:
+//        1 x 3 = 3
+//        2 x 3 = 6
+//        3 x 3 = 9
+//……
+//        9 x 3 = 27
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        for (int i = 1; i < 10; i++){
+            System.out.println(i + " x " + k + " = " + i*k);
+        }
     }
 }
