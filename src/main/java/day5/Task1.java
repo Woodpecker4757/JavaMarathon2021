bdf4108c5d9a34e22a69d96cc00c76507bc3de1c
Woodpecker <agubaevt@mail.ru>
2022-02-23 07:46:16 +0600

    23/02/2022 7:45:37.80

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,12 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car ();
+        car.setColor("black");
+        car.setYear(2000);
+        car.setModel("audi");
+
+        System.out.println("Our car " + car.getYear() + "year " + car.getModel() + " model " + car.getColor() + " color ");
 
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,8 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+//        2. Создать класс Мотоцикл (англ. Motorbike) с полями “Модель”, “Цвет”, “Год выпуска”. Создать экземпляр класса Мотоцикл, используя конструктор (set методы не использовать). Необходимо придерживаться принципов инкапсуляции и использовать ключевое слово this. Вывести в консоль значение каждого из полей, используя get методы.
+        Motorbike motorbike = new Motorbike(2010,"kawasaki","red");
+        System.out.println(motorbike.getModel() + "\n" + motorbike.getYear() + "\n" + motorbike.getColor());
     }
 }
