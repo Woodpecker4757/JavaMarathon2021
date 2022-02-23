d71dca32c60e2fb4572a7852da8a8f9a3ceb4a9c
Woodpecker <agubaevt@mail.ru>
2022-02-23 17:02:33 +0600

    23/02/2022 17:02:11.69

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -1,7 +1,28 @@
 package day8;
+//1. Создать строку, состоящую из чисел от 0 до 20000. Важно понимать, что это одна строка, полученная конкатенацией (“склеиванием”) чисел из диапазона через пробел (0 + “ “ + 1 + “ “ + 2 + … + 20000).
+//        После создания такой строки, вызов System.out.println() на этой строке должен вывести в консоль сразу все числа из диапазона:
+//        0 1 2 3 4 5 6 7 8 9 10 11 12 … 19995 19996 19997 19998 19999 20000
+//
+//        Для того, чтобы почувствовать разницу в производительности между конкатенацией обычных строк (класс String) и использовании StringBuilder, реализуйте описанную задачу этими двумя способами, замеряя время работы программы.
 
 public class Task1 {
     public static void main(String[] args) {
 
+        String text = "";
+        long before = System.currentTimeMillis();
+        for(int i = 0;i <= 2000;i++){
+            text += i + " ";
+        }
+        long after = System.currentTimeMillis();
+        System.out.println(after - before);
+        System.out.println(text);
+        StringBuilder sb = new StringBuilder("");
+        long before2 = System.currentTimeMillis();
+        for(int i = 0;i <= 2000;i++){
+            sb.append(i).append(" ");
+        }
+        long after2 = System.currentTimeMillis();
+        System.out.println(sb);
+        System.out.println(after2 - before2);
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -1,7 +1,15 @@
 package day8;
-
+//2. Скопируйте класс Самолет из задания дня 6.
+//        Внесите изменения в класс таким образом, чтобы следующий код выводил информацию о самолете, аналогично вызову метода info().
+//
+//        Airplane airplane = new Airplane ("Boeing", 2000, 150, 10000);
+//        System.out.println(airplane);
 public class Task2 {
     public static void main(String[] args) {
-
+        long before = System.currentTimeMillis();
+        Plane plane = new Plane("Boeng",2000,150,10000);
+        System.out.println(plane);
+        long after = System.currentTimeMillis();
+        System.out.println(after - before);
     }
 }
