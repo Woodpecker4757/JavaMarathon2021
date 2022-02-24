9324f93bb2965e0bc72c4b889e5cf1fea3804c40
Woodpecker <agubaevt@mail.ru>
2022-02-25 04:03:22 +0600

    25/02/2022 4.02:24.59

--- src/main/java/day12/Task1.java
+++ src/main/java/day12/Task1.java
@@ -1,7 +1,19 @@
 package day12;
 
+import java.util.ArrayList;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add(new String("Audi"));
+        arrayList.add(new String("BMW"));
+        arrayList.add(new String("Lada"));
+        arrayList.add(new String("Tesla"));
+        arrayList.add(new String("Mercedes"));
+        System.out.println(arrayList);
+        int center = arrayList.size()/2;
+        arrayList.add(center,"Kia");
+        System.out.println(arrayList);
+        arrayList.remove(0);
     }
 }
--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -1,7 +1,23 @@
 package day12;
 
+import java.util.ArrayList;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        int fs = 0;
+        int fe = 30;
+        int ss = 300;
+        int se = 350;
+        boolean fwas = true;
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = fs; i <= se;i++){
+            arrayList.add(i);
+            if(i == fe && fwas){
+                i=ss;
+                arrayList.add(i);
+                fwas = false;
+            }
+        }
+        System.out.println(arrayList);
     }
 }
