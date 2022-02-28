980b04cffbaf5e10df1d3f86f5ae9da73f0e7035
Woodpecker <agubaevt@mail.ru>
2022-02-28 13:58:14 +0600

    28/02/2022 13.57:59.77

--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -1,7 +1,15 @@
 package day13;
 
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
+        User user1 = new User("Alberto");
+        User user2 = new User("Sebastyan");
+        user2.sendMessage(user1,"Oh,hello there!");
+        user1.sendMessage(user2,"How are you?");
+        user2.sendMessage(user1,"Cool!");
+        user1.sendMessage(user2,"Does it work?");
+        user2.sendMessage(user1,"Yes");
+        MessageDatabase.showDialog(user1,user2);
     }
 }
---
+++ src/main/java/day13/User.java
@@ -0,0 +1,55 @@
+package day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String username;
+    private List<User> subscriptions;
+
+    public User(String username) {
+        this.username = username;
+        this.subscriptions = new ArrayList<>();
+    }
+
+    public void subscribe(User user){
+        this.subscriptions.add(user);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<User> getSubscriptions() {
+        return subscriptions;
+    }
+
+    public void setSubscriptions(List<User> subscriptions) {
+        this.subscriptions = subscriptions;
+    }
+
+    public boolean isSubscribed(User user){
+        for(User currentUser : subscriptions){
+            if(currentUser.getUsername().equals(user.getUsername()));
+                return true;
+        }
+
+        return false;
+    }
+
+    public boolean isFriend(User user){
+        return this.isSubscribed(user) && user.isSubscribed(this);
+    }
+
+    public void sendMessage(User user,String text){
+        MessageDatabase.addNewMessage(this, user, text);
+    }
+
+    public String toString(){
+        return username;
+    }
+}
